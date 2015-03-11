a8583b24e704f4c4afb5ffd676e636fe93823c7a
hunternope <joris.rijkes@student.hu.nl>
2015-03-11 15:02:22 +0100

    Hoofdmenu toegevoegd (scripts, plaatjes, scene) Pause menu gerefactored met inheritance van menu.cs Nieuwe scene (hoofdmenu) toegeveogd Oude scene "testmap" veranderd veranderd naar "Level01"

---
+++ MyRTSGame/Assets/Menu/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTS;
+
+public class MainMenu : Menu {
+	
+	protected override void SetButtons () {
+		buttons = new string[] {"New Game", "Exit Game"};
+	}
+	
+	protected override void HandleButton (string text) {
+		switch(text) {
+		case "New Game": NewGame(); break;
+		case "Exit Game": ExitGame(); break;
+		default: break;
+		}
+	}
+	
+	private void NewGame() {
+		ResourceManager.MenuOpen = false;
+		Application.LoadLevel("Level01");
+		//makes sure that the loaded level runs at normal speed
+		Time.timeScale = 1.0f;
+	}
+}
--- MyRTSGame/Assets/Menu/Scripts/PauseMenu.cs
+++ MyRTSGame/Assets/Menu/Scripts/PauseMenu.cs
@@ -1,51 +1,31 @@
 ﻿using UnityEngine;
 using RTS;
 
-public class PauseMenu : MonoBehaviour {
-	
-	public GUISkin GUIBackgroundSkin;
-	public Texture2D header;
+public class PauseMenu : Menu {
 	
 	private Player player;
-	private string[] buttons = {"Resume", "Exit Game"};
 	
-	void Start () {
+	protected override void Start () {
+		base.Start();
 		player = transform.root.GetComponent< Player >();
 	}
 	
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Escape)) Resume();
 	}
-
-	void OnGUI() {
-		GUI.skin = GUIBackgroundSkin;
-		
-		float groupLeft = Screen.width / 2 - ResourceManager.MenuWidth / 2;
-		float groupTop = Screen.height / 2 - ResourceManager.PauseMenuHeight / 2;
-		GUI.BeginGroup(new Rect(groupLeft, groupTop, ResourceManager.MenuWidth, ResourceManager.PauseMenuHeight));
-		
-		//background box
-		GUI.Box(new Rect(0, 0, ResourceManager.MenuWidth, ResourceManager.PauseMenuHeight), "");
-		//header image
-		GUI.DrawTexture(new Rect(ResourceManager.Padding, ResourceManager.Padding, ResourceManager.HeaderWidth, ResourceManager.HeaderHeight), header);
-		
-		//menu buttons
-		float leftPos = ResourceManager.MenuWidth / 2 - ResourceManager.ButtonWidth / 2;
-		float topPos = 2 * ResourceManager.Padding + ResourceManager.HeaderHeight;
-		for(int i = 0; i < buttons.Length; i++) {
-			if(i > 0) topPos += ResourceManager.ButtonHeight + ResourceManager.Padding;
-			if(GUI.Button(new Rect(leftPos, topPos, ResourceManager.ButtonWidth, ResourceManager.ButtonHeight), buttons[i])) {
-				switch(buttons[i]) {
-				case "Resume": Resume(); break;
-				case "Exit Game": ExitGame(); break;
-				default: break;
-				}
-			}
-		}
-		
-		GUI.EndGroup();
+	
+	protected override void SetButtons () {
+		buttons = new string[] {"Resume", "Quit Game"};
 	}
-
+	
+	protected override void HandleButton (string text) {
+		switch(text) {
+		case "Resume": Resume(); break;
+		case "Quit Game": ReturnToMainMenu(); break;
+		default: break;
+		}
+	}
+	
 	private void Resume() {
 		Time.timeScale = 1.0f;
 		GetComponent< PauseMenu >().enabled = false;
@@ -54,7 +34,10 @@ public class PauseMenu : MonoBehaviour {
 		ResourceManager.MenuOpen = false;
 	}
 
-	private void ExitGame() {
-		Application.Quit();
+	private void ReturnToMainMenu() {
+		Application.LoadLevel("MainMenu");
+		Screen.showCursor = true;
 	}
+
+	
 }
